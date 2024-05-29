490d9054f87688a490e7aae3c077e605cf83ac0f
coralinesoul <coraline.soul@gmail.com>
2024-05-29 15:49:10 +0200

    ADD lliens to etude and form

--- resources/views/catalogue/find.blade.php
+++ resources/views/catalogue/find.blade.php
@@ -7,6 +7,13 @@
         <h1>{{$etude->title}}</h1>
         <h2> {{$etude->longtitle}} </h2>
         <p> {{$etude->resume}} </p>
+
+        @foreach($etude->liens as $lien)
+            <div>
+                <strong>{{ $lien->position }}. {{ $lien->link_name }}:</strong>
+                <a href="{{ $lien->link_url }}">{{ $lien->link_url }}</a>
+            </div>
+        @endforeach
     </etude>   
 
 @endsection
