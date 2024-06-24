b363dc18be7655123bf6f0f03d91e259336092ea
coralinesoul <coraline.soul@gmail.com>
2024-06-24 16:56:49 +0200

    add gp / matrice / parametre

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -37,6 +37,36 @@ class CatalogueController extends Controller
             $q->whereIn('id', $themeIds);
         });
     }
+    
+    if ($request->has('groupeParametre')) {
+        $groupeIds = $request->get('groupeParametre');
+        $parametreIds = Parametre::whereIn('groupe', $groupeIds)->pluck('id')->toArray();
+        $query->whereHas('parametres', function($q) use ($parametreIds) {
+            $q->whereIn('id', $parametreIds);
+        });
+    }
+
+    if ($request->has('parametre')) {
+        $parametreIds = $request->get('parametre');
+        $query->whereHas('parametres', function($q) use ($parametreIds) {
+            $q->whereIn('id', $parametreIds);
+        });
+    }
+
+    if ($request->has('groupeMatrice')) {
+        $groupeMIds = $request->get('groupeMatrice');
+        $matriceIds = Matrice::whereIn('groupe', $groupeMIds)->pluck('id')->toArray();
+        $query->whereHas('matrices', function($q) use ($matriceIds) {
+            $q->whereIn('id', $matriceIds);
+        });
+    }
+
+    if ($request->has('matrice')) {
+        $matriceIds = $request->get('matrice');
+        $query->whereHas('matrices', function($q) use ($matriceIds) {
+            $q->whereIn('id', $matriceIds);
+        });
+    }
 
     if ($request->has('zone')) {
         $zoneIds = $request->get('zone');
@@ -44,6 +74,18 @@ class CatalogueController extends Controller
             $q->whereIn('id', $zoneIds);
         });
     }
+
+    if ($request->filled('reglementaire')) {
+        $reglementaireFilters = (array) $request->get('reglementaire', []);
+        if (in_array('1', $reglementaireFilters) && in_array('0', $reglementaireFilters)) {
+
+        } elseif (in_array('1', $reglementaireFilters)) {
+            $query->where('reglementaire', true);
+        } elseif (in_array('0', $reglementaireFilters)) {
+            $query->where('reglementaire', false);
+        }
+    }
+
     if ($request->has('min_year') && $request->has('max_year')) {
         $minYear = $request->get('min_year');
         $maxYear = $request->get('max_year');
@@ -61,12 +103,19 @@ class CatalogueController extends Controller
     $allSources = Source::select('id', 'name')->get(); 
     $allThemes = Theme::select('id', 'name')->get(); 
     $allZones = Zone::select('id', 'name')->get(); 
-
+    $allParametres = Parametre::select('id', 'name','groupe')->get(); 
+    $allMatrices = Matrice::select('id', 'name','groupe')->get(); 
+    $groupesUniques = $allParametres->unique('groupe');
+    $groupesMatrices = $allMatrices->unique('groupe');
     return view('catalogue.index', [
         'etudes' => $etudes,
         'allSources' => $allSources,
         'allThemes'=>$allThemes,
         'allZones'=>$allZones,
+        'allParametres'=>$allParametres,
+        'allMatrices'=>$allMatrices,
+        'groupesUniques'=>$groupesUniques,
+        'groupesMatrices'=>$groupesMatrices,
     ]);
 }
     public function create() {
