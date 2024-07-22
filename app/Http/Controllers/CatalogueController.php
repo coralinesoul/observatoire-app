1bc515f4bf8535c463d637eb3b62aa050b05d848
coralinesoul <coraline.soul@gmail.com>
2024-07-22 12:44:52 +0200

    add MtoM filters

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -20,104 +20,11 @@ use Illuminate\Support\Str;
 
 class CatalogueController extends Controller
 {
-    public function index(Request $request)
-{
-    $query = Etude::query();
-
-    if ($request->has('source')) {
-        $sourceIds = $request->get('source');
-        $query->whereHas('sources', function($q) use ($sourceIds) {
-            $q->whereIn('id', $sourceIds);
-        });
-    }
-
-    if ($request->has('theme')) {
-        $themeIds = $request->get('theme');
-        $query->whereHas('themes', function($q) use ($themeIds) {
-            $q->whereIn('id', $themeIds);
-        });
+    public function index()
+    {
+        return view('catalogue.index');
     }
     
-    if ($request->has('groupeParametre')) {
-        $groupeIds = $request->get('groupeParametre');
-        $parametreIds = Parametre::whereIn('groupe', $groupeIds)->pluck('id')->toArray();
-        $query->whereHas('parametres', function($q) use ($parametreIds) {
-            $q->whereIn('id', $parametreIds);
-        });
-    }
-
-    if ($request->has('parametre')) {
-        $parametreIds = $request->get('parametre');
-        $query->whereHas('parametres', function($q) use ($parametreIds) {
-            $q->whereIn('id', $parametreIds);
-        });
-    }
-
-    if ($request->has('groupeMatrice')) {
-        $groupeMIds = $request->get('groupeMatrice');
-        $matriceIds = Matrice::whereIn('groupe', $groupeMIds)->pluck('id')->toArray();
-        $query->whereHas('matrices', function($q) use ($matriceIds) {
-            $q->whereIn('id', $matriceIds);
-        });
-    }
-
-    if ($request->has('matrice')) {
-        $matriceIds = $request->get('matrice');
-        $query->whereHas('matrices', function($q) use ($matriceIds) {
-            $q->whereIn('id', $matriceIds);
-        });
-    }
-
-    if ($request->has('zone')) {
-        $zoneIds = $request->get('zone');
-        $query->whereHas('zones', function($q) use ($zoneIds) {
-            $q->whereIn('id', $zoneIds);
-        });
-    }
-
-    if ($request->filled('reglementaire')) {
-        $reglementaireFilters = (array) $request->get('reglementaire', []);
-        if (in_array('1', $reglementaireFilters) && in_array('0', $reglementaireFilters)) {
-
-        } elseif (in_array('1', $reglementaireFilters)) {
-            $query->where('reglementaire', true);
-        } elseif (in_array('0', $reglementaireFilters)) {
-            $query->where('reglementaire', false);
-        }
-    }
-
-    if ($request->has('min_year') && $request->has('max_year')) {
-        $minYear = $request->get('min_year');
-        $maxYear = $request->get('max_year');
-        $query->where(function($q) use ($minYear, $maxYear) {
-            $q->whereBetween('startyear', [$minYear, $maxYear])
-              ->orWhereBetween('stopyear', [$minYear, $maxYear])
-              ->orWhere(function($q) use ($minYear, $maxYear) {
-                  $q->where('startyear', '<=', $minYear)
-                    ->where('stopyear', '>=', $maxYear);
-              });
-        });
-    }
-
-    $etudes = $query->paginate(4);
-    $allSources = Source::select('id', 'name')->get(); 
-    $allThemes = Theme::select('id', 'name')->get(); 
-    $allZones = Zone::select('id', 'name')->get(); 
-    $allParametres = Parametre::select('id', 'name','groupe')->get(); 
-    $allMatrices = Matrice::select('id', 'name','groupe')->get(); 
-    $groupesUniques = $allParametres->unique('groupe');
-    $groupesMatrices = $allMatrices->unique('groupe');
-    return view('catalogue.index', [
-        'etudes' => $etudes,
-        'allSources' => $allSources,
-        'allThemes'=>$allThemes,
-        'allZones'=>$allZones,
-        'allParametres'=>$allParametres,
-        'allMatrices'=>$allMatrices,
-        'groupesUniques'=>$groupesUniques,
-        'groupesMatrices'=>$groupesMatrices,
-    ]);
-}
     public function create() {
         $etude = new Etude();
         return view('catalogue.create',[
--- app/Http/Livewire/Filter.php
+++ app/Http/Livewire/Filter.php
@@ -3,34 +3,103 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use App\Models\Etude;
-use App\Models\Source;
+use \App\Models\Etude;
+use \App\Models\Source;
+use \App\Models\Type;
+use \App\Models\Zone;
+use \App\Models\Theme;
+use \App\Models\Parametre;
+use \App\Models\Matrice;
+
 
 class Filter extends Component
 {
-    public $selectedOption = [];
     public $etudes;
+
+    public $selectedTheme = [];
+    public $selectedSource = [];
+    public $selectedParametre = [];
+    public $selectedMatrice = [];
+    public $selectedZone = [];
+    public $selectedType = [];
+    public $selectedReglementaire =[];
+
+    public $themes;
+    public $sources;
+    public $parametres;
+    public $matrices;
+    public $zones;
+    public $types;
 
     public function mount()
     {
+        $this->themes = Theme::all();
+        $this->sources = Source::all();
+        $this->parametres = Parametre::all();
+        $this->matrices = Matrice::all();
+        $this->zones = Zone::all();
+        $this->types = Type::all();
+
         $this->getselect();
     }
 
     public function getselect()
+    
     {
-        if (empty($this->selectedOption)) {
-            // Si aucune source n'est sélectionnée, retourner toutes les études avec leurs sources
-            $this->etudes = Etude::with('sources')->get();
+        if (empty($this->selectedSource) && empty($this->selectedTheme)&& empty($this->selectedParametre)&& empty($this->selectedMatrice)&& empty($this->selectedZone)&& empty($this->selectedType)&& empty($this->selectedReglementaire)) {
+            // Si aucune source et aucun thème ne sont sélectionnés, retourner toutes les études avec leurs sources et thèmes
+            $this->etudes = Etude::with('sources', 'themes','parametres','matrices','zones','types')->get();
         } else {
-            // Filtrer les études en fonction des sources sélectionnées
-            $this->etudes = Etude::with('sources')->whereHas('sources', function ($query) {
-                $query->whereIn('id', $this->selectedOption);
-            })->get();
+            // Filtrer les études en fonction des sources et des thèmes sélectionnés
+            $this->etudes = Etude::with('sources', 'themes','parametres','matrices','zones','types')
+                ->when(!empty($this->selectedSource), function ($query) {
+                    $query->whereHas('sources', function ($query) {
+                        $query->whereIn('id', $this->selectedSource);
+                    });
+                })
+                ->when(!empty($this->selectedTheme), function ($query) {
+                    $query->whereHas('themes', function ($query) {
+                        $query->whereIn('id', $this->selectedTheme);
+                    });
+                })
+                ->when(!empty($this->selectedParametre), function ($query) {
+                    $query->whereHas('parametres', function ($query) {
+                        $query->whereIn('id', $this->selectedParametre);
+                    });
+                })
+                ->when(!empty($this->selectedMatrice), function ($query) {
+                    $query->whereHas('matrices', function ($query) {
+                        $query->whereIn('id', $this->selectedMatrice);
+                    });
+                })
+                ->when(!empty($this->selectedZone), function ($query) {
+                    $query->whereHas('zones', function ($query) {
+                        $query->whereIn('id', $this->selectedZone);
+                    });
+                })
+                ->when(!empty($this->selectedType), function ($query) {
+                    $query->whereHas('types', function ($query) {
+                        $query->whereIn('id', $this->selectedType);
+                    });
+                })
+                ->when(!empty($this->selectedReglementaire), function ($query) {
+                    $query->whereIn('reglementaire', $this->selectedReglementaire);
+                })
+            
+                ->get();
         }
     }
 
     public function render()
     {
-        return view('livewire.filter', ['etudes' => $this->etudes]);
+
+        return view('livewire.filter', 
+            ['etudes' => $this->etudes, 
+            'sources' => $this->sources, 
+            'parametres' => $this->parametres,
+            'matrices' => $this->matrices,
+            'zones' => $this->zones,
+            'types' => $this->types]
+        );
     }
 }
