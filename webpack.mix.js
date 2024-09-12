27fb34ee8e81b63a06112551f7455e99875bf490
coralinesoul <coraline.soul@gmail.com>
2024-09-12 09:50:05 +0200

    Script migrate

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
 .js('resources/js/filter.js', 'public/js')
+.js('resources/js/form.js', 'public/js')
 .postCss('resources/css/app.css', 'public/css', [
     tailwindcss('./tailwind.config.js'),
     require('autoprefixer'),
