a8ff2a7e0b510f5bdefb3ec92bee8a0ed1fbdda3
coralinesoul <coraline.soul@gmail.com>
2024-09-24 16:26:03 +0200

    Add pagination index

--- app/Http/Livewire/Filter.php
+++ app/Http/Livewire/Filter.php
@@ -3,25 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\Etude;
-use App\Models\Source;
-use App\Models\Type;
-use App\Models\Zone;
 use App\Models\Theme;
+use App\Models\Source;
 use App\Models\Parametre;
 use App\Models\Matrice;
+use App\Models\Type;
+use App\Models\Zone;
 
 class Filter extends Component
 {
-    public $etudes;
+    use WithPagination;
 
-    public $selectedTheme = [];
+    public $sources;
+    public $themes;
+    public $parametres;
+    public $matrices;
+    public $zones;
+    public $types;
+
     public $selectedSource = [];
-    public $selectedGp = [];
-    public $selectedGpM = [];
-
+    public $selectedTheme = [];
     public $selectedParametre = [];
     public $selectedMatrice = [];
+    public $selectedGp = [];
+    public $selectedGpM = [];
     public $selectedZone = [];
     public $selectedType = [];
     public $selectedReglementaire = [];
@@ -29,108 +36,27 @@ class Filter extends Component
     public $selectedStartyear = 1960;
     public $selectedStopyear = 2024;
 
-    public $themes;
-    public $sources;
-    public $parametres;
+
     public $filteredParametres;
     public $filteredGp;
     public $filteredMatrices;
     public $filteredGpM;
-    public $matrices;
-    public $zones;
-    public $types;
 
     public function mount()
     {
-        $this->themes = Theme::all();
+        // Charger toutes les sources disponibles au montage du composant
         $this->sources = Source::all();
+        $this->themes = Theme::all();
         $this->parametres = Parametre::all();
         $this->matrices = Matrice::all();
+        $this->filteredGp = $this->parametres->pluck('groupe')->unique();
+        $this->filteredGpM = $this->matrices->pluck('groupe')->unique();
         $this->zones = Zone::all();
         $this->types = Type::all();
-
-        $this->filteredGp = $this->parametres->pluck('groupe')->unique();
-        $this->filteredGpM = $this->matrices->pluck('groupe')->unique(); // Initialize with all matrix groups
-        $this->getselect();
-    }
-
-    public function getselect()
-    {
-        if (empty($this->selectedSource)  
-            && empty($this->selectedTheme) 
-            && empty($this->selectedParametre)
-            && empty($this->selectedGp)
-            && empty($this->selectedGpM)
-            && empty($this->selectedMatrice)
-            && empty($this->selectedZone)
-            && empty($this->selectedType)
-            && empty($this->selectedReglementaire)
-            && empty($this->selectedFrequence)
-            && ($this->selectedStartyear == 1960 && $this->selectedStopyear == 2024)) {
-            // Si aucune source et aucun thème ne sont sélectionnés, retourner toutes les études avec leurs sources et thèmes
-            $this->etudes = Etude::with('sources', 'themes', 'parametres', 'matrices', 'zones', 'types')->get();
-        } else {
-            // Filtrer les études en fonction des sources et des thèmes sélectionnés
-            $this->etudes = Etude::with('sources', 'themes', 'parametres', 'matrices', 'zones', 'types')
-                ->when(!empty($this->selectedSource), function ($query) {
-                    $query->whereHas('sources', function ($query) {
-                        $query->whereIn('id', $this->selectedSource);
-                    });
-                })
-                ->when(!empty($this->selectedTheme), function ($query) {
-                    $query->whereHas('themes', function ($query) {
-                        $query->whereIn('id', $this->selectedTheme);
-                    });
-                })
-                ->when(!empty($this->selectedGp), function ($query) {
-                    $query->whereHas('parametres', function($query) {
-                        $query->whereIn('groupe', $this->selectedGp);
-                    });
-                })
-                ->when(!empty($this->selectedParametre), function ($query) {
-                    $query->whereHas('parametres', function ($query) {
-                        $query->whereIn('id', $this->selectedParametre);
-                    });
-                })
-                ->when(!empty($this->selectedGpM), function ($query) {
-                    $query->whereHas('matrices', function($query) {
-                        $query->whereIn('groupe', $this->selectedGpM);
-                    });
-                })
-                ->when(!empty($this->selectedMatrice), function ($query) {
-                    $query->whereHas('matrices', function ($query) {
-                        $query->whereIn('id', $this->selectedMatrice);
-                    });
-                })
-                ->when(!empty($this->selectedZone), function ($query) {
-                    $query->whereHas('zones', function ($query) {
-                        $query->whereIn('id', $this->selectedZone);
-                    });
-                })
-                ->when(!empty($this->selectedType), function ($query) {
-                    $query->whereHas('types', function ($query) {
-                        $query->whereIn('id', $this->selectedType);
-                    });
-                })
-                ->when(!empty($this->selectedReglementaire), function ($query) {
-                    $query->whereIn('reglementaire', $this->selectedReglementaire);
-                })
-                ->when(!empty($this->selectedFrequence), function ($query) {
-                    $query->whereIn('frequence', $this->selectedFrequence);
-                })
-                ->when($this->selectedStartyear != 1960 || $this->selectedStopyear != 2024, function ($query) {
-                    $query->where('startyear', '>=', $this->selectedStartyear)
-                          ->where('stopyear', '<=', $this->selectedStopyear);
-                })
-                ->get();
-        }
     }
 
     public function updateFilteredOptions()
     {
-        // Mettre à jour la liste des études en fonction des filtres actuels
-        $this->getselect();
-
         // Initialiser les variables de filtre
         $filteredGp = collect();
         $filteredParametres = collect();
@@ -187,19 +113,78 @@ class Filter extends Component
 
         // Filtrer les paramètres pour s'assurer qu'ils sont associés à une étude
         $this->filteredParametres = $filteredParametres->filter(function($parametre) {
-            return $this->etudes->contains(function($etude) use ($parametre) {
+            return $this->getSelect()->contains(function($etude) use ($parametre) {
                 return $etude->parametres->contains('id', $parametre->id);
+                });
             });
-        });
+        
+            // Filtrer les matrices pour s'assurer qu'elles sont associées à une étude
+            $this->filteredMatrices = $filteredMatrices->filter(function($matrice) {
+                return $this->getSelect()->contains(function($etude) use ($matrice) {
+                    return $etude->matrices->contains('id', $matrice->id);
+                });
+            });
 
-        // Filtrer les matrices pour s'assurer qu'elles sont associées à une étude
-        $this->filteredMatrices = $filteredMatrices->filter(function($matrice) {
-            return $this->etudes->contains(function($etude) use ($matrice) {
-                return $etude->matrices->contains('id', $matrice->id);
-            });
-        });
+        $this->resetPage();
+
     }
 
+    public function getSelect()
+    {
+        // Récupérer les études filtrées par source 'IECP' avec pagination
+        return Etude::with('sources', 'themes', 'parametres', 'matrices', 'zones', 'types')
+            ->when(!empty($this->selectedSource), function ($query) {
+                $query->whereHas('sources', function ($query) {
+                    $query->whereIn('id', $this->selectedSource);
+                });
+            })
+            ->when(!empty($this->selectedTheme), function ($query) {
+                $query->whereHas('themes', function ($query) {
+                    $query->whereIn('id', $this->selectedTheme);
+                });
+            })
+            ->when(!empty($this->selectedGp), function ($query) {
+                $query->whereHas('parametres', function($query) {
+                    $query->whereIn('groupe', $this->selectedGp);
+                });
+            })
+            ->when(!empty($this->selectedParametre), function ($query) {
+                $query->whereHas('parametres', function ($query) {
+                    $query->whereIn('id', $this->selectedParametre);
+                });
+            })
+            ->when(!empty($this->selectedGpM), function ($query) {
+                $query->whereHas('matrices', function($query) {
+                    $query->whereIn('groupe', $this->selectedGpM);
+                });
+            })
+            ->when(!empty($this->selectedMatrice), function ($query) {
+                $query->whereHas('matrices', function ($query) {
+                    $query->whereIn('id', $this->selectedMatrice);
+                });
+            })
+            ->when(!empty($this->selectedZone), function ($query) {
+                $query->whereHas('zones', function ($query) {
+                    $query->whereIn('id', $this->selectedZone);
+                });
+            })
+            ->when(!empty($this->selectedType), function ($query) {
+                $query->whereHas('types', function ($query) {
+                    $query->whereIn('id', $this->selectedType);
+                });
+            })
+            ->when($this->selectedStartyear != 1960 || $this->selectedStopyear != 2024, function ($query) {
+                $query->where('startyear', '>=', $this->selectedStartyear)
+                      ->where('stopyear', '<=', $this->selectedStopyear);
+            })
+            ->when(!empty($this->selectedReglementaire), function ($query) {
+                $query->whereIn('reglementaire', $this->selectedReglementaire);
+            })
+            ->when(!empty($this->selectedFrequence), function ($query) {
+                $query->whereIn('frequence', $this->selectedFrequence);
+            })
+            ->paginate(16);
+    }
     public function removeSelection($type, $value)
     {
         // Supprime la sélection correspondante en fonction du type
@@ -235,15 +220,21 @@ class Filter extends Component
         $this->updateFilteredOptions();
     }
 
+
+
     public function render()
     {
+        // Utilisation de la méthode pour récupérer les études filtrées
+        $etudes = $this->getSelect();
+
         return view('livewire.filter', [
-            'etudes' => $this->etudes,
+            'etudes' => $etudes,
             'sources' => $this->sources,
+            'themes' => $this->themes,
             'parametres' => $this->filteredParametres,
             'matrices' => $this->filteredMatrices,
             'zones' => $this->zones,
-            'types' => $this->types
+            'types' => $this->types,
         ]);
     }
 }
