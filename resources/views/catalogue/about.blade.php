17f5e871f84bc908bfbe4932cf309cff2add9e67
coralinesoul <coraline.soul@gmail.com>
2024-10-08 14:09:21 +0200

    Footer

--- resources/views/catalogue/about.blade.php
+++ resources/views/catalogue/about.blade.php
@@ -6,7 +6,7 @@
     <br>
     <div class="w-full h-[80vh]">
         <input
-        class="hidden peer/slider1 checkbox"
+        class="hidden peer/slider1 checkbox "
         type="radio"
         name="slider"
         id="slider1"
@@ -28,7 +28,7 @@
         <div
         class="relative w-[300vw] h-[100%] flex transition-all duration-500 ease-in-out peer-checked/slider1:-left-0 peer-checked/slider2:-left-[100vw] peer-checked/slider3:-left-[200vw]"
         >
-        <div class="relative w-full h-full flex p-8">
+        <div class="relative w-full h-full flex p-8 rounded-none bg-blue2 bg-opacity-5 shadow-md p-6">
             <h1 class="text-blue1 font-bold text-2xl mt-2">TEST</h1>
             <h1 class="text-blue1 font-bold text-2xl mt-2">Faire l'inventaire des données</h1>
 
