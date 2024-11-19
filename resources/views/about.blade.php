c6e2399c8bfa5829a4f6ef371b7e94def11180bc
coralinesoul <coraline.soul@gmail.com>
2024-11-19 15:46:56 +0100

    MAJ About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,10 +2,10 @@
 @section ('title',"L'observatoire")
 @section ('content')
 
-    <div class="flex flex-col md:flex-row p-10 px-20">
+    <div class=" p-10 sm:px-8 md:px-20 lg:px-30">
         <div class="basis-2/3">
-            <h1 class="text-3xl font-bold tracking-wide text-blue2 mt-1">L'institut écocitoyen</h1>
-            <br>
+            <a class="text-3xl font-bold tracking-wide text-blue2 mt-1 mb-8" href="https://www.institut-ecocitoyen.fr/">L'institut écocitoyen</a>
+            <br><br>
             <p class="font-serif">
                 Créé en 2010, l’Institut Écocitoyen est un centre de recherche scientifique sur l’environnement et sur l’effet des pollutions sur la santé humaine. Grâce à un partenariat historique avec les organismes universitaires en région (LCE, MIO, CEREGE, IMBE...), l’Institut Écocitoyen crée une interface entre enjeux territoriaux et travaux de recherche. Son expérience sur l’ensemble des domaines environnementaux et sanitaires autour du golfe de Fos et sa localisation à Fos-sur-Mer lui confère une place centrale dans le dispositif de l’observatoire.
 Son rôle sera d’animer le comité de pilotage de l’observatoire. Il aura également pour mission d’assurer la centralisation des connaissances sur les milieux marins et littoraux et la bancarisation des données correspondantes. Afin de poursuivre et pérenniser le développement de la connaissance sur l’état de la masse d’eau du golfe de Fos, de ses littoraux et des lagunes côtières du périmètre, ainsi que pour maintenir des capacités d’intervention en situation accidentelle, l’observatoire devra assurer le maintien des moyens matériels et humains.
@@ -13,9 +13,28 @@ Ses moyens matériels comprennent : une sonde multiparamètres (type CTD), un c
 L’Institut Écocitoyen dispose également de moyens de plongée destinés au PMT (palme-masque-tuba) et d’un agent titulaire de l’aptitude à travailler en milieu hyperbare niveau B0. Les capacités de navigation sont assurés grâce aux réseaux locaux et citoyens, qui permettent à l’Institut Ecocitoyen de réagir très rapidement avec les moyens à la mer ou de plongée (avec ou sans bouteille) nécessaires.
             </p>
         </div>
-        <div class="basis-1/3 flex justify-center items-start p-10">
-            <img src="{{asset('test.jpg')}}" class="rounded-lg w-64 h-auto" alt="Image">
-        </div>
+        <br>
+<div class="relative overflow-hidden">
+    <div class="flex animate-slide" style="gap: 0;">
+        <!-- Images -->
+        <img src="{{asset('/Materiel/CTD.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/filet_manta.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/labo.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/plongee.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/Niskin.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/sedimento.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <!-- Duplication des images pour créer un effet de boucle -->
+        <img src="{{asset('/Materiel/CTD.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/filet_manta.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/labo.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/plongee.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+        <img src="{{asset('/Materiel/Niskin.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
+    </div>
+</div>
+
+        
+        
+        
     </div>
     <div class="flex flex-col md:flex-row p-10 px-20">
         <div class="basis-2/3">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,30 @@ module.exports = {
       colors: {
         'blue1': '#185064',
         'blue2': '#80BCB3',
-        'blue3':'#f3f8f6',
+        'blue3': '#f3f8f6',
+      },
+      animation: {
+        slide: 'slide 20s linear infinite', // Animation fluide sur 20 secondes
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0px)' },
+          '10%': { transform: 'translateX(0px)' }, // Pause pour le premier groupe
+          '15%': { transform: 'translateX(-332px)' }, // Décalage vers la deuxième image
+          '25%': { transform: 'translateX(-332px)' }, // Pause pour le deuxième groupe
+          '30%': { transform: 'translateX(-664px)' }, // Décalage vers la troisième image
+          '40%': { transform: 'translateX(-664px)' }, // Pause pour le troisième groupe
+          '45%': { transform: 'translateX(-996px)' }, // Décalage vers la quatrième image
+          '55%': { transform: 'translateX(-996px)' }, // Pause pour le quatrième groupe
+          '60%': { transform: 'translateX(-1328px)' }, // Décalage vers la cinquième image
+          '70%': { transform: 'translateX(-1328px)' }, // Pause pour le cinquième groupe
+          '75%': { transform: 'translateX(-1660px)' }, // Décalage vers la sixième image
+          '85%': { transform: 'translateX(-1660px)' }, // Pause pour la sixième image
+          '90%': { transform: 'translateX(-1992px)' }, // Décalage vers les images dupliquées
+          '100%': { transform: 'translateX(-1992px)' }, // Retour fluide à la boucle
+        },
       },
     },
   },
   plugins: [],
-}
-
+};
