9707c5c3b2b8a327498000f0a195e7ad1481f512
coralinesoul <coraline.soul@gmail.com>
2024-11-19 16:17:32 +0100

    Carrousel arrows

--- resources/views/livewire/carousel.blade.php
+++ resources/views/livewire/carousel.blade.php
@@ -1,6 +1,15 @@
-<div class=" bg-blue3 -mx-8">
-    <div x-data="{ currentSlide: @entangle('currentSlide'), interval: null }"
-        x-init="interval = setInterval(() => { currentSlide = (currentSlide + 1) % {{ count($slideViews) }} }, 30000)"
+<div class="bg-blue3 -mx-8">
+    <div x-data="{
+            currentSlide: @entangle('currentSlide'),
+            slideCount: {{ count($slideViews) }},
+            goToNext() {
+                this.currentSlide = (this.currentSlide + 1) % this.slideCount;
+            },
+            goToPrevious() {
+                this.currentSlide = (this.currentSlide - 1 + this.slideCount) % this.slideCount;
+            }
+        }"
+        x-init="setInterval(() => goToNext(), 30000)"
         class="relative w-full h-screen md:h-[80vh] lg:h-[75vh]">
 
         <!-- Diapositive 1 -->
@@ -30,9 +39,25 @@
             @include('livewire.carousel.slide3')
         </div>
 
+        <!-- Flèche gauche -->
+        <button @click="goToPrevious()"
+            class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-blue1 text-white rounded-full p-2 hover:bg-blue2 focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+        </button>
+
+        <!-- Flèche droite -->
+        <button @click="goToNext()"
+            class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-blue1 text-white rounded-full p-2 hover:bg-blue2 focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+        </button>
+
         <!-- Indicateurs de navigation -->
         <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-            <template x-for="index in 3" :key="index">
+            <template x-for="index in slideCount" :key="index">
                 <span @click="currentSlide = index - 1"
                     :class="{'bg-blue1': currentSlide === index - 1, 'bg-gray-300': currentSlide !== index - 1}"
                     class="w-3 h-3 rounded-full cursor-pointer transition-colors duration-300">
