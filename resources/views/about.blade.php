81336eabdcb622cfc3403bd868f46024a49d2eaf
coralinesoul <coraline.soul@gmail.com>
2024-11-20 11:57:43 +0100

    MAJ MEP about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,56 +7,87 @@
             <a class="text-3xl font-bold tracking-wide text-blue2 mt-1 mb-8" href="https://www.institut-ecocitoyen.fr/">L'institut écocitoyen</a>
             <br><br>
             <p class="font-serif">
-                Créé en 2010, l’Institut Écocitoyen est un centre de recherche scientifique sur l’environnement et sur l’effet des pollutions sur la santé humaine. Grâce à un partenariat historique avec les organismes universitaires en région (LCE, MIO, CEREGE, IMBE...), l’Institut Écocitoyen crée une interface entre enjeux territoriaux et travaux de recherche. Son expérience sur l’ensemble des domaines environnementaux et sanitaires autour du golfe de Fos et sa localisation à Fos-sur-Mer lui confère une place centrale dans le dispositif de l’observatoire.
-Son rôle sera d’animer le comité de pilotage de l’observatoire. Il aura également pour mission d’assurer la centralisation des connaissances sur les milieux marins et littoraux et la bancarisation des données correspondantes. Afin de poursuivre et pérenniser le développement de la connaissance sur l’état de la masse d’eau du golfe de Fos, de ses littoraux et des lagunes côtières du périmètre, ainsi que pour maintenir des capacités d’intervention en situation accidentelle, l’observatoire devra assurer le maintien des moyens matériels et humains.
-Ses moyens matériels comprennent : une sonde multiparamètres (type CTD), un courantomètre ADCP, une bouteille de prélèvement d’eau Niskin, une benne à sédiments Van Veen, un filet à plancton, un filet manta à microplastiques, un débitmètre à hélice (...). Il dispose en outre d’un laboratoire de préparation d’échantillons et d’observation microscopique (loupe binoculaire et microscope optique) et de moyens de stockage et de conservation d’échantillons, qui pourront être utilisés en fonction des disponibilités, des moyens humains et des financements d’études ou de suivis.
-L’Institut Écocitoyen dispose également de moyens de plongée destinés au PMT (palme-masque-tuba) et d’un agent titulaire de l’aptitude à travailler en milieu hyperbare niveau B0. Les capacités de navigation sont assurés grâce aux réseaux locaux et citoyens, qui permettent à l’Institut Ecocitoyen de réagir très rapidement avec les moyens à la mer ou de plongée (avec ou sans bouteille) nécessaires.
+                Créé en 2010, l’Institut Écocitoyen est un centre de recherche scientifique sur l’<span class="font-bold text-blue1">effet des pollutions sur l’environnement et la santé humaine</span>. Grâce à un partenariat historique avec les organismes universitaires de la région (LCE, MIO, CEREGE, IMBE...), l’Institut crée une interface entre <span class="font-bold text-blue1">enjeux territoriaux et travaux de recherche</span>. Son expérience sur l’ensemble des domaines environnementaux et sanitaires autour du golfe de Fos et sa localisation à Fos-sur-Mer lui confère une place centrale dans le dispositif de cet Observatoire.
+                <br>
+                Il anime et organise le <span class="font-bold text-blue1">Comité de Pilotage de l’Observatoire</span>, réalise la maîtrise d’oeuvre d’études spécifiques au golfe de Fos et assure la <span class="font-bold text-blue1">centralisation des connaissances</span> sur les milieux marins et littoraux et la <span class="font-bold text-blue1">bancarisation</span> des données correspondantes. 
+                <br>
+                Les moyens matériels de l’Institut comprennent notamment deux sondes multi-paramètres (type CTD), un courantomètre ADCP, une bouteille Niskin de prélèvement d’eau, une benne à sédiments Van Veen, un filet à plancton, un filet manta pour les microplastiques, . Il dispose également d’un laboratoire de préparation d’échantillons et d’observation microscopique (loupe binoculaire et microscope optique) et de moyens de stockage et de conservation d’échantillons, qui pourront être <span class="font-bold text-blue1">mis à disposition en fonction des disponibilités, et des ressources</span>.
             </p>
         </div>
         <br>
-<div class="relative overflow-hidden">
-    <div class="flex animate-slide" style="gap: 0;">
-        <!-- Images -->
-        <img src="{{asset('/Materiel/CTD.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/filet_manta.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/labo.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/plongee.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/Niskin.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/sedimento.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <!-- Duplication des images pour créer un effet de boucle -->
-        <img src="{{asset('/Materiel/CTD.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/filet_manta.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/labo.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/plongee.JPG')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-        <img src="{{asset('/Materiel/Niskin.jpg')}}" class="rounded-lg w-80 h-80 mr-3" alt="Image">
-    </div>
-</div>
+        <div class="relative overflow-hidden">
+            <div class="flex animate-slide gap-3">
+                <!-- Images avec titres -->
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/CTD.jpg') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Sonde CTD</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/filet_manta.jpg') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Filet Manta</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/labo.JPG') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Laboratoire de l'institut</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/plongee.JPG') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Matériel de plongée</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/Niskin.jpg') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Bouteille de prélèvement d'eau</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/sedimento.JPG') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Benne à sédiments</h3>
+                </div>
+                <!-- Duplication des images pour effet de boucle -->
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/CTD.jpg') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Sonde CTD</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/filet_manta.jpg') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Filet Manta</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/labo.JPG') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Laboratoire de l'institut</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/plongee.JPG') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Matériel de plongée</h3>
+                </div>
+                <div class="relative flex-shrink-0 w-80" style="width: 20rem; height: 24rem;">
+                    <img src="{{ asset('/Materiel/Niskin.jpg') }}" class="w-full h-4/5 rounded-lg object-cover" alt="Image">
+                    <h3 class="text-center text-blue1 mt-2 font-bold">Bouteille de prélèvement d'eau</h3>
+                </div>
+            </div>
+        </div>
 
-        
-        
-        
     </div>
-    <div class="flex flex-col md:flex-row p-10 px-20">
+    <div class="flex flex-col md:flex-row pb-10 px-20">
         <div class="basis-2/3">
-            <h1 class="text-3xl font-bold tracking-wide text-blue2 mt-1">Contact</h1>
+            <h1 class="text-3xl font-bold tracking-wide text-blue2 mt-1">Nous contacter</h1>
             <br>
             <p class="font-serif">
                 Institut Ecocitoyen pour la Connaissance des Pollutions
             </br>
-                Centre de vie la Fossette RD 268 </br>
+                Centre de Vie la Fossette - RD 268</br>
                 13270 Fos-sur-Mer </br>
                 France </br>
-                Mail : contact@observatoire-golfe.fr ;  contact@institut-ecocitoyen.fr</br>
+                Mails : contact@observatoire-golfe.fr </br>
                 
                 <br>
-                Pour toutes demande, veuillez vous adressez au standard au 04 90 55 49 94.
+                Pour toutes demandes, veuillez vous adresser au standard au 04 90 55 49 94.
             </p>
         </div>
     </div>
     
 </div>    
     <div class="px-28">
-        <h1 class="text-3xl font-bold tracking-wide text-blue2 mt-1">Les partenaires</h1>
+        <h1 class="text-3xl font-bold tracking-wide text-blue2 mt-1">Soutiens financiers</h1>
         <br>
         <br>
         <div class="flex items-center justify-between space-x-8 mt-4 lg:px-64 md:px-32">
