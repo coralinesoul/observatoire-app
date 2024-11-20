a3865e288d063c9f4e55026243fe423ba478ad63
coralinesoul <coraline.soul@gmail.com>
2024-11-20 15:18:49 +0100

    Bold txt home

--- resources/views/livewire/carousel/slide1.blade.php
+++ resources/views/livewire/carousel/slide1.blade.php
@@ -4,8 +4,8 @@
     <livewire:etude-counter />
     <br>
     <p class="font-serif">
-        L'Observatoire du golfe de Fos a pour premier objectif de regrouper les connaissances environnementales existantes. Dans cette perceptive, le catalogue a été créé pour accéder librement aux études identifiées sur le golfe de Fos. Il s'appuie sur une base de données permettant de filtrer sa recherche selon des besoins spécifiques. Afin de disposer de l’inventaire le plus complet possible en intégrant la grande diversité des connaissances et de leurs producteurs, chaque étude est référencée dans une page permettant d’accéder aux données produites, aux contacts, aux rapports ou articles disponibles. 
+        L'Observatoire du golfe de Fos a pour premier objectif de <span class="font-bold text-blue1">regrouper les connaissances environnementales existantes</span>. Dans cette perceptive, le catalogue a été créé pour accéder librement aux études identifiées sur le golfe de Fos. Il s'appuie sur une base de données permettant de filtrer sa recherche selon des besoins spécifiques. Afin de disposer de l’inventaire le plus complet possible en intégrant la <span class="font-bold text-blue1">grande diversité des connaissances</span> et de leurs producteurs, chaque étude est référencée dans une page permettant d’accéder aux données produites, aux contacts, aux rapports ou articles disponibles. 
         <br>
-        Cet inventaire est dynamique et les différents actrices et acteurs  du golfe de Fos peuvent bénéficier d’un compte et ainsi intégrer leurs propres informations. 
+        Cet inventaire est <span class="font-bold text-blue1">dynamique</span> et les différents actrices et acteurs du golfe de Fos peuvent bénéficier d’un compte et ainsi intégrer <span class="font-bold text-blue1">leurs propres informations</span>. 
     </p>
 </div>
